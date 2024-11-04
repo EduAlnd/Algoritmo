8c366c7fe84bb48e4cd4e53928b539d960596b53
EduardaLinux <ifeduarda.luiz@gmail.com>
2024-11-04 03:12:16 -0300

    Retirando o git Ignore

---
+++ repeticao/exApostila/EXs/26/26-W.c
@@ -0,0 +1,15 @@
+/*Foi feita uma pesquisa de audiência de canal de TV em várias casas de uma certa cidade, e mum
+determinado dia. Para cada casa visitada foram fornecidos o número do canal (4, 5, 7, 12) e o
+número de pessoas que estavam assistindo a ele naquela casa. Se a televisão estivesse desligada,
+nada seria anotado, ou seja, esta casa não entraria na pesquisa. Criar um programa que:
+• Leia um número indeterminado de dados, isto é, o número do canal e o número de pessoas que
+estavam assistindo;
+• Calcule e imprima a porcentagem de audiência em cada canal.
+Obs.: Para encerrar a entrada de dados, digite o número do canal zero.*/
+
+#include <stdio.h>
+int main(){
+
+
+    return 0;
+}
