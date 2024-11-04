0f8603bb48a042ff1b6eaff79f5e527e277a8df2
EduardaLinux <ifeduarda.luiz@gmail.com>
2024-11-04 03:07:33 -0300

    erro nome

--- repeticao/exApostila/EXs/11/11-W.c
+++
@@ -1,9 +0,0 @@
-/*Construa um programa em C que leia vários números e informe quantos números entre 100 e
-200 foram digitados. Quando o valor 0 (zero) for lido, o algoritmo deverá cessar sua execução.*/
-
-#include <stdio.h>
-int main(){
-
-
-    return 0;
-}
