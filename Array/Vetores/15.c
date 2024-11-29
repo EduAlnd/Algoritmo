69e6fa1a1f87c399f17a392a5cc1e8e43ce4b651
Duda <ifeduarda.luiz@gmail.com>
2024-11-28 22:00:16 -0300

    Array exercicios

--- Array/Vetores/15.c
+++ Array/Vetores/15.c
@@ -3,8 +3,25 @@
 números é 100.*/
 
 #include <stdio.h>
-#define max 5
+#define max 100
 int main(){
-
+    int vet[max], l=0, count=0, i, iguais=0;
+	
+	printf("Entre com um numero:\n");
+	do{
+		scanf("%d", &vet[l]);
+		if(vet[l]==0){
+			count++;
+		}
+		l++;
+	}while((count==0) && (l < max));
+	l-=2;
+	for(i=0; i < l; i++){
+        if(vet[i] == vet[l]){
+        	iguais++;
+		}
+    }
+    printf("Existem %d numeros iguais ao ultimo numero digitado.", iguais);
+	
     return 0;
 }
--- Array/Vetores/16.c
+++ Array/Vetores/16.c
@@ -6,6 +6,31 @@
 #include <stdio.h>
 #define max 5
 int main(){
-
+    float conj[max];
+	int l, igual = 0, maior = 0, igualM = 0, media=0;
+	printf("Entre com um valor para o conjunto:\n");
+	for(l=0; l<max; l++) {
+		scanf("%f", &conj[l]);
+		media += conj[l];
+	}
+	media/= max;
+	for(l=0; l<max; l++) {
+		printf("%.2f ", conj[l]);
+	}
+	printf("%d ", media);
+	for(l=0; l<max; l++) {
+		if(conj[l]==30) {
+			igual++;
+		}
+		if(conj[l] > media) {
+			maior++;
+		}
+		if(conj[l] == media) {
+			igualM++;
+		}
+	}
+	printf("Numeros iguais a 30: %d\n", igual);
+	printf("Numeros maiores que a media: %d\n", maior);
+	printf("Numeros iguais media: %d\n", igualM);
     return 0;
 }
